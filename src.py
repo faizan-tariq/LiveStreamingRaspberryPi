0e3466b25a220ae43a4af096fb76644b024fa3d0
Faizan Tariq <faizaan.sagitarion@gmail.com>
2019-12-09 20:55:52 -0600

    Create src.py

---
+++ src.py
@@ -0,0 +1,13 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+camera.start_preview()
+camera.start_recording('/home/pi/Desktop/video.h264')
+camera.capture('/home/pi/Desktop/image.jpg')
+
+sleep(5)
+
+camera.stop_recording()
+camera.stop_preview()
